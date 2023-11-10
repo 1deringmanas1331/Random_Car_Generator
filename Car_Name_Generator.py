ffaca37246fcafce86d564b735d597501893dd73
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-10 19:23:46 +0530

    Update Car_Name_Generator.py

--- Car_Name_Generator.py
+++ Car_Name_Generator.py
@@ -1,13 +1,13 @@
 import random
 
-# List of car names
+# List of super car names
 cars = [
     "Bugatti Veyron", "Lamborghini Aventador", "Ferrari LaFerrari", "McLaren P1", "Koenigsegg Jesko", "Porsche 911 GT2 RS", "Aston Martin Valkyrie", "Pagani Huayra", "Rimac C_Two", "Lotus Evija", "Audi R8", "Nissan GT-R", "Mercedes-AMG One", "McLaren Senna", "Ferrari 812 Superfast", "Lamborghini Huracán", "Bugatti Chiron", "Porsche 918 Spyder", "Aston Martin One-77", "Koenigsegg Regera", "Pagani Zonda", "Lamborghini Veneno", "McLaren 720S", "Ferrari F8 Tributo", "Bugatti EB110", "Lamborghini Diablo", "Aston Martin DBS Superleggera", "McLaren GT", "Porsche Carrera GT", "Koenigsegg Gemera", "Pagani Imola", "Ferrari SF90 Stradale", "Lamborghini Centenario", "Aston Martin V12 Vantage", "Nissan Nismo GT-R", "Porsche 911 Turbo S", "Koenigsegg Agera", "Bugatti Type 57SC Atlantic", "McLaren 675LT", "Ferrari Enzo", "Lamborghini Reventón", "Pagani Huayra Roadster", "Aston Martin DB11", "Koenigsegg One:1", "Porsche 911 GT3 RS", "McLaren MP4-12C", "Ferrari 488 Pista", "Lamborghini Murciélago", "Aston Martin Vantage", "Pagani Huayra BC", "Koenigsegg CCX", "Porsche 911 GT2", "McLaren 570S", "Ferrari 458 Italia", "Lamborghini Sesto Elemento", "Aston Martin Vulcan", "Pagani Zonda Cinque", "Koenigsegg CCXR", "Porsche 911 GT3", "McLaren 650S", "Ferrari Testarossa", "Lamborghini Gallardo", "Aston Martin Zagato", "Pagani Zonda Roadster", "Koenigsegg Trevita", "Porsche 911 R", "McLaren F1", "Ferrari 288 GTO", "Lamborghini Countach", "Aston Martin V8 Vantage", "Pagani Zonda HP Barchetta", "Koenigsegg CC8S", "Porsche 911 Targa", "McLaren 600LT", "Ferrari 250 GTO", "Lamborghini LM002", "Aston Martin Rapide", "Pagani Zonda Tricolore", "Koenigsegg CCR", "Porsche 911 Carrera", "McLaren 540C", "Ferrari 250 GT Lusso", "Lamborghini Jalpa", "Aston Martin Lagonda", "Pagani Zonda 760", "Koenigsegg Jesko Absolut", "Porsche 911 Speedster", "McLaren 12C", "Ferrari F40", "Lamborghini Miura", "Aston Martin DBS", "Pagani Zonda Cinque Roadster", "Koenigsegg Regera Ghost", "Porsche 911 GTS", "McLaren 675LT Spider", "Ferrari 330 P4", "Lamborghini 350 GT", "Aston Martin Cygnet", "Pagani Zonda Revolucion", "Koenigsegg Gemera R"
 ]
    
 
 
-# Generate a random car name
+# Generates a random car name
 random_car = random.choice(cars)
 
 # Print the random car name
